28329342286179758d75d7e4a6a52382274839ee
joe-sc-ms <sc-vlg612936@microsoft.com>
2024-12-03 07:56:09 -0800

    cleanup

--- src/components/LoadingBar.tsx
+++
@@ -1,9 +0,0 @@
-import loadingBarGif from "../assets/loading-bar.gif";
-
-export const LoadingBar = () => {
-  return (
-    <div className="loading-bar">
-      <img src={loadingBarGif} alt="loading" />
-    </div>
-  );
-};
