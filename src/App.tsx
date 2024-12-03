9e918c8c84653bdb09983ff0687d714f40c4c2a7
joe-sc-ms <sc-vlg612936@microsoft.com>
2024-12-03 07:47:00 -0800

    asset buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import ChatBot from "./features/ChatBot";
+import { NextUIProvider } from "@nextui-org/system";
 import "./App.css";
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <ChatBot />
-    </div>
+    <NextUIProvider>
+      <div className="app-wrapper">
+        <ChatBot />
+      </div>
+    </NextUIProvider>
   );
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,16 @@
-import { SENDER } from "./constants";
+import { Message, ROLE } from "./constants";
 
-export const createMessage = (user: SENDER, text: string) => {
+export const createMessage = (user: ROLE, text: string): Message => {
   return {
-    sender: user,
-    text: text,
+    role: user,
+    content: text,
     time: new Date().toLocaleString(),
   };
+};
+
+export const filterAssetList = (text: string): string[] => {
+  const sectionStart = "hostnames-referenced:";
+  if (text.includes(sectionStart)) {
+    return text.split(sectionStart);
+  }
 };
